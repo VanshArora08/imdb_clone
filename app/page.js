4b68b72994714352e64b82014bbc48c9bcae662e
VanshArora08 <kumar.vansharora@gmail.com>
2023-06-20 18:51:46 +0530

    movies data fetching done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 // import React from 'react'
 // import { Children } from 'react/cjs/react.production.min'
+import Results from "@/components/Results"
+const API_KEY=process.env.TMDB_API_KEY
 
-const Home = ( ) => {
+const Home = async ({searchParams} ) => {
+  const genre= searchParams.genre || "fetchTrending"
+  const res= await fetch(`https://api.themoviedb.org/3/${genre==="fetchTopRated"?"movie/top_rated":"trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10000 }})
+  if(!res.ok){
+    throw new Error("failed to fetch data")
+  }
+  const data= await res.json()
+  const results=data.results;
+
+  // console.log(results);
   return (
     <div>
-      {/* HOME */}
+      <Results results={results}/>
     </div>
   ) 
 }
